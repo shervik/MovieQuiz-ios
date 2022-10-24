6a37029eb436e15168d0c7f1963d504943fc43fa
Victoria Scherbakova <gesvik5@gmail.com>
2022-10-24 15:01:39 +0300

    убран сториборд

--- MovieQuiz/Helpers/UIFont+Extensions.swift
+++ MovieQuiz/Helpers/UIFont+Extensions.swift
@@ -9,6 +9,8 @@ import Foundation
 import UIKit
 
 extension UIFont {
-    static let ysDisplayMedium = UIFont(name: "YSDisplay-Medium", size: 20) ?? UIFont()
-    static let ysDisplayBold = UIFont(name: "YSDisplay-Bold", size: 23) ?? UIFont()
+    static let ysDisplayMedium = UIFont(name: "YSDisplay-Medium", size: 20) ??
+    UIFont.systemFont(ofSize: 20, weight: Weight(500))
+    static let ysDisplayBold = UIFont(name: "YSDisplay-Bold", size: 23) ??
+    UIFont.systemFont(ofSize: 23, weight: Weight(700))
 }
